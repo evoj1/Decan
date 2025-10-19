716a50478d3eed3cb39eb4e3a9169f20e91b4583
AfE <evoj1cehovich@gmail.com>
2025-10-19 17:01:45 +0700

    Add LocalDB

--- Model/Student.cs
+++ Model/Student.cs
@@ -8,14 +8,9 @@ namespace Model
 {
     public class Student
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Speciality { get; set; }
         public string Group { get; set; }
-        public Student(string name, string speciality, string group)
-        {
-            Name = name;
-            Speciality = speciality;
-            Group = group;
-        }
     }
 }
